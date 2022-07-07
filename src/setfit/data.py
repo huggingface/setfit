b950607e9201d054d01fa57e80527dfd395f9070
Lewis Tunstall <lewis@huggingface.co>
2022-07-07 13:04:09 +0000

    Fix tests

--- src/setfit/data.py
+++ src/setfit/data.py
@@ -5,6 +5,7 @@ from datasets import Dataset, DatasetDict
 
 
 SEEDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+SAMPLE_SIZES = [2, 4, 8, 16, 32, 64]
 
 
 def create_samples(df: pd.DataFrame, sample_size: int, seed: int) -> pd.DataFrame:
