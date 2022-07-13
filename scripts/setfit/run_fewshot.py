19442819e9ca59c4869028044ff482b66f3e25ea
Daniel Korat <daniel.korat@intel.com>
2022-07-13 14:52:39 +0300

    decouple train/eval continued; clean train_eval_all_datasets(); add type hints

--- scripts/setfit/run_fewshot.py
+++ scripts/setfit/run_fewshot.py
@@ -6,21 +6,22 @@ import os
 import pathlib
 import sys
 from shutil import copyfile
+from typing import Dict, Tuple
+from typing_extensions import LiteralString
 from warnings import simplefilter
 
 import numpy as np
-from datasets import load_dataset
+from datasets import Dataset, DatasetDict, load_dataset
 from evaluate import load
-from sentence_transformers import InputExample, losses
+from sentence_transformers import SentenceTransformer, InputExample, losses
 from sentence_transformers.datasets import SentenceLabelDataset
 from sentence_transformers.losses.BatchHardTripletLoss import BatchHardTripletLossDistanceFunction
-from setfit_wrapper import SetFit
 from sklearn.linear_model import LogisticRegression
 from torch.utils.data import DataLoader
 from utils import DEV_DATASET_TO_METRIC, TEST_DATASET_TO_METRIC
 
 from setfit.data import SAMPLE_SIZES, create_fewshot_splits
-from setfit.modeling import LOSS_NAME_TO_CLASS, SKLearnWrapper, SupConLoss, sentence_pairs_generation
+from setfit.modeling import LOSS_NAME_TO_CLASS, SetFitModel, SKLearnWrapper, SupConLoss, sentence_pairs_generation
 
 
 # ignore all future warnings
@@ -51,13 +52,14 @@ def parse_args():
     parser.add_argument("--lr", type=float, default=0.001)
     parser.add_argument("--is_dev_set", type=bool, default=False)
     parser.add_argument("--is_test_set", type=bool, default=False)
+    parser.add_argument("--override_results", default=False, action="store_true")
     args = parser.parse_args()
 
     return args
 
 
 class RunFewShot:
-    def __init__(self, args) -> None:
+    def __init__(self, args: argparse.Namespace) -> None:
         # Prepare directory for results
         self.args = args
 
@@ -89,73 +91,38 @@ class RunFewShot:
         self.loss_class = LOSS_NAME_TO_CLASS[args.loss]
 
         # Load SetFit Model
-        self.model_wrapper = SetFit(
+        self.model_wrapper = SetFitModel(
             self.args.model, max_seq_length=args.max_seq_length, add_normalization_layer=args.add_normalization_layer
         )
         self.model = self.model_wrapper.model
 
-    def eval(self, x_train, y_train, x_test, y_test, metric):
-        """Computes the metrics for a given classifier."""
-        # Define metrics
-        metric_fn = load(metric)
-
-        clf = self.get_classifier(self.model)
-        clf.fit(x_train, y_train)
-        y_pred = clf.predict(x_test)
-
-        metrics = metric_fn.compute(predictions=y_pred, references=y_test)
-        print(f"{metric} -- {metrics[metric]:.4f}")
-
-        return metrics
-
-    def get_classifier(self, sbert_model):
+    def get_classifier(self, sbert_model: SentenceTransformer) -> SKLearnWrapper:
         if self.args.classifier == "logistic_regression":
             return SKLearnWrapper(sbert_model, LogisticRegression())
 
-    def load_data_splits(self, dataset):
+    def load_data_splits(self, dataset: str) -> Tuple[DatasetDict, Dataset]:
         """Loads a dataset from the Hugging Face Hub and returns the test split and few-shot training splits."""
         print(f"\n\n\n============== {dataset} ============")
         # Load one of the SetFit training sets from the Hugging Face Hub
         train_split = load_dataset(f"SetFit/{dataset}", split="train")
-        few_shot_train_splits = create_fewshot_splits(train_split, self.args.sample_sizes)
+        train_splits = create_fewshot_splits(train_split, self.args.sample_sizes)
         test_split = load_dataset(f"SetFit/{dataset}", split="test")
         print(f"Test set: {len(test_split)}")
-        return few_shot_train_splits, test_split
+        return train_splits, test_split
 
-    def train_eval_all_datasets(self):
-        for dataset, metric in self.dataset_to_metric.items():
-            few_shot_train_splits, test_split = self.load_data_splits(dataset)
-
-            for split_name, split_data in few_shot_train_splits.items():
-                results_path = os.path.join(self.output_path, dataset, split_name, "results.json")
-                print(f"\n\n======== {os.path.dirname(results_path)} =======")
-                os.makedirs(os.path.dirname(results_path), exist_ok=True)
-                if os.path.exists(results_path):
-                    print(f"Skipping finished experiment: {results_path}")
-                    continue
-                
-                x_train = split_data["text"]
-                y_train = split_data["label"]
-
-                self.train(x_train, y_train, self.loss_class, self.args.num_epochs)
-
-                x_test = test_split["text"]
-                y_test = test_split["label"]
-
-                metrics = self.eval(x_train, y_train, x_test, y_test, metric)
-
-                with open(results_path, "w") as f_out:
-                    json.dump({"score": metrics[metric] * 100, "measure": metric}, f_out, sort_keys=True)
-
-    def train(self, x_train, y_train, loss_class, num_epochs):
+    def train(self, data: Dataset) -> SKLearnWrapper:
+        "Trains a SetFit model on the given few-shot training data."
         self.model.load_state_dict(copy.deepcopy(self.model_wrapper.model_original_state))
 
-        if loss_class is None:
+        x_train = data["text"]
+        y_train = data["label"]
+
+        if self.loss_class is None:
             return
 
         # sentence-transformers adaptation
         batch_size = self.args.batch_size
-        if loss_class in [
+        if self.loss_class in [
             losses.BatchAllTripletLoss,
             losses.BatchHardTripletLoss,
             losses.BatchSemiHardTripletLoss,
@@ -169,25 +136,25 @@ class RunFewShot:
             batch_size = min(self.args.batch_size, len(train_data_sampler))
             train_dataloader = DataLoader(train_data_sampler, batch_size=batch_size, drop_last=True)
 
-            if loss_class is losses.BatchHardSoftMarginTripletLoss:
-                train_loss = loss_class(
+            if self.loss_class is losses.BatchHardSoftMarginTripletLoss:
+                train_loss = self.loss_class(
                     model=self.model, distance_metric=BatchHardTripletLossDistanceFunction.cosine_distance
                 )
-            elif loss_class is SupConLoss:
-                train_loss = loss_class(model=self.model)
+            elif self.loss_class is SupConLoss:
+                train_loss = self.loss_class(model=self.model)
             else:
-                train_loss = loss_class(
+                train_loss = self.loss_class(
                     model=self.model, distance_metric=BatchHardTripletLossDistanceFunction.cosine_distance, margin=0.25
                 )
 
-            train_steps = len(train_dataloader) * num_epochs
+            train_steps = len(train_dataloader) * self.args.num_epochs
         else:
             train_examples = []
-            for _ in range(num_epochs):
+            for _ in range(self.args.num_epochs):
                 train_examples = sentence_pairs_generation(np.array(x_train), np.array(y_train), train_examples)
 
             train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=batch_size)
-            train_loss = loss_class(self.model)
+            train_loss = self.loss_class(self.model)
             train_steps = len(train_dataloader)
 
         print(f"{len(x_train)} train samples in total, {train_steps} train steps with batch size {batch_size}")
@@ -200,6 +167,53 @@ class RunFewShot:
             warmup_steps=warmup_steps,
             show_progress_bar=False,
         )
+
+        # Train the final classifier
+        classifier = self.get_classifier(self.model)
+        classifier.fit(x_train, y_train)
+        return classifier
+
+
+    def eval(self, classifier: SKLearnWrapper, data: Dict[str,str], metric: str) -> dict:
+        """Computes the metrics for a given classifier."""
+        # Define metrics
+        metric_fn = load(metric)
+
+        x_test = data["text"]
+        y_test = data["label"]
+
+        y_pred = classifier.predict(x_test)
+
+        metrics = metric_fn.compute(predictions=y_pred, references=y_test)
+        print(f"{metric} -- {metrics[metric]:.4f}")
+
+        return metrics
+
+    def create_results_path(self, dataset: str, split_name: str) -> LiteralString:
+        results_path = os.path.join(self.output_path, dataset, split_name, "results.json")
+        print(f"\n\n======== {os.path.dirname(results_path)} =======")
+        os.makedirs(os.path.dirname(results_path), exist_ok=True)
+        return results_path
+
+    def train_eval_all_datasets(self) -> None:
+        """Trains and evaluates the model on each split for every dataset."""
+        for dataset, metric in self.dataset_to_metric.items():
+            few_shot_train_splits, test_data = self.load_data_splits(dataset)
+
+            for split_name, train_data in few_shot_train_splits.items():
+                results_path = self.create_results_path(dataset, split_name)
+                if os.path.exists(results_path) and not self.args.override_results:
+                    print(f"Skipping finished experiment: {results_path}")
+                    continue                
+                
+                # Train the model on the current train split
+                classifier = self.train(train_data)
+
+                # Evaluate the model on the test data
+                metrics = self.eval(classifier, test_data, metric)
+
+                with open(results_path, "w") as f_out:
+                    json.dump({"score": metrics[metric] * 100, "measure": metric}, f_out, sort_keys=True)
 
 
 def main():
