f0806062e998eb7556e849930b9a213c2e40936b
oren.pereg <oren.pereg@intel.com>
2022-07-14 10:12:25 +0300

    style updates

--- scripts/setfit/run_few_shot_distillation.py
+++ scripts/setfit/run_few_shot_distillation.py
@@ -6,14 +6,13 @@ import os
 import pathlib
 import sys
 from shutil import copyfile
-from warnings import simplefilter
 from typing import List
+from warnings import simplefilter
 from xmlrpc.client import Boolean
-import pandas as pd
-
 
 import numpy as np
-from datasets import load_dataset,Dataset, DatasetDict
+import pandas as pd
+from datasets import Dataset, DatasetDict, load_dataset
 from evaluate import load
 from sentence_transformers import InputExample
 from setfit_wrapper import SetFit
@@ -23,14 +22,16 @@ from torch.utils.data import DataLoader
 from utils import DEV_DATASET_TO_METRIC, TEST_DATASET_TO_METRIC
 
 from setfit.data import SAMPLE_SIZES
-from setfit.modeling import LOSS_NAME_TO_CLASS, SKLearnWrapper, SupConLoss, sentence_pairs_generation
+from setfit.modeling import LOSS_NAME_TO_CLASS, SKLearnWrapper, sentence_pairs_generation
 
-TEACHER=0
-TEACHER_SEED= [0]
-STUDENT=1
-STUDENT_SEEDS= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+TEACHER = 0
+TEACHER_SEED = [0]
+STUDENT = 1
+STUDENT_SEEDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # ignore all future warnings
 simplefilter(action="ignore", category=FutureWarning)
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -62,26 +63,27 @@ def parse_args():
 
     return args
 
+
 def create_samples(df: pd.DataFrame, sample_size: int, seed: int, mode) -> pd.DataFrame:
     """Samples a DataFrame to create an equal number of samples per class (when possible)."""
     examples = []
-    if mode==TEACHER:
+    if mode == TEACHER:
         for label in df["label"].unique():
             subset = df.query(f"label == {label}")
             if len(subset) > sample_size:
                 examples.append(subset.sample(sample_size, random_state=seed, replace=False))
             else:
                 examples.append(subset)
-                
-        examples = pd.concat(examples)   
-                
-    if mode==STUDENT:  
+
+        examples = pd.concat(examples)
+
+    if mode == STUDENT:
         examples = df.sample(sample_size, random_state=seed, replace=False)
-               
+
     return examples
 
 
-def create_fewshot_splits(dataset: Dataset, sample_sizes: List[int], seeds, mode:Boolean) -> DatasetDict:
+def create_fewshot_splits(dataset: Dataset, sample_sizes: List[int], seeds, mode: Boolean) -> DatasetDict:
     """Creates training splits from the dataset with an equal number of samples per class (when possible)."""
     splits_ds = DatasetDict()
     df = dataset.to_pandas()
@@ -91,44 +93,46 @@ def create_fewshot_splits(dataset: Dataset, sample_sizes: List[int], seeds, mode
             splits_ds[f"train-{sample_size}-{idx}"] = Dataset.from_pandas(split_df, preserve_index=False)
     return splits_ds
 
+
 def sentence_pairs_generation_cos_sim(sentences, pairs, cos_sim_matrix):
-	# initialize two empty lists to hold the (sentence, sentence) pairs and
-	# labels to indicate if a pair is positive or negative
+    # initialize two empty lists to hold the (sentence, sentence) pairs and
+    # labels to indicate if a pair is positive or negative
 
-  idx = list(range(len(sentences)))
+    idx = list(range(len(sentences)))
 
-  for idxA in range(len(sentences)):      
-    currentSentence = sentences[idxA]
-    idxB = int(np.random.choice([x for x in idx if x!=idxA]))
+    for idxA in range(len(sentences)):
+        currentSentence = sentences[idxA]
+        idxB = int(np.random.choice([x for x in idx if x != idxA]))
 
-    cos_sim=float(cos_sim_matrix[idxA][idxB])
-    pairedSentence = sentences[idxB]
-    pairs.append(InputExample(texts=[currentSentence, pairedSentence], label=cos_sim))
+        cos_sim = float(cos_sim_matrix[idxA][idxB])
+        pairedSentence = sentences[idxB]
+        pairs.append(InputExample(texts=[currentSentence, pairedSentence], label=cos_sim))
 
-    idxC = np.random.choice([x for x in idx if x!=idxA])
-    cos_sim=float(cos_sim_matrix[idxA][idxC])
-    pairedSentence = sentences[idxC]
-    pairs.append(InputExample(texts=[currentSentence, pairedSentence], label=cos_sim))
-    
-  return (pairs) 
+        idxC = np.random.choice([x for x in idx if x != idxA])
+        cos_sim = float(cos_sim_matrix[idxA][idxC])
+        pairedSentence = sentences[idxC]
+        pairs.append(InputExample(texts=[currentSentence, pairedSentence], label=cos_sim))
+
+    return pairs
+
 
 class RunFewShot:
     def __init__(self, args, mode, trained_teacher_model, x_train_teacher) -> None:
         # Prepare directory for results
         self.args = args
 
-        if mode==TEACHER:
+        if mode == TEACHER:
             model = args.teacher_model
             path_prefix = f"distil_teacher_{args.teacher_model.replace('/', '-')}"
             self.mode = TEACHER
-         
-        if mode==STUDENT:
+
+        if mode == STUDENT:
             model = args.student_model
             path_prefix = f"distil_student_{args.student_model.replace('/', '-')}"
             self.trained_teacher_model = trained_teacher_model
             self.x_train_teacher = x_train_teacher
             self.mode = STUDENT
-                
+
         parent_directory = pathlib.Path(__file__).parent.absolute()
         self.output_path = (
             parent_directory
@@ -158,8 +162,10 @@ class RunFewShot:
 
         # Load SetFit Model
         self.model_wrapper = SetFit(
-            #self.args.model, max_seq_length=args.max_seq_length, add_normalization_layer=args.add_normalization_layer
-            model, max_seq_length=args.max_seq_length, add_normalization_layer=args.add_normalization_layer
+            # self.args.model, max_seq_length=args.max_seq_length, add_normalization_layer=args.add_normalization_layer
+            model,
+            max_seq_length=args.max_seq_length,
+            add_normalization_layer=args.add_normalization_layer,
         )
         self.model = self.model_wrapper.model
 
@@ -184,29 +190,33 @@ class RunFewShot:
     def train(self):
         for dataset, metric in self.dataset_to_metric.items():
             if self.mode == TEACHER:
-                print(f"\n\n\n=========== Training Teacher =========")
+                print("\n\n\n=========== Training Teacher =========")
             else:
-                print(f"\n\n\n======== Training SetFit Student ======")
+                print("\n\n\n======== Training SetFit Student ======")
             print(f"\n\n\n============== {dataset} ============")
             # Load one of the SetFit training sets from the Hugging Face Hub
             train_ds = load_dataset(f"SetFit/{dataset}", split="train")
             test_dataset = load_dataset(f"SetFit/{dataset}", split="test")
             print(f"Test set: {len(test_dataset)}")
-            
+
             # if teacher training use only 1 split (send only 1 seed. seed= 0)
-            if self.mode==TEACHER:
-                fewshot_ds = create_fewshot_splits(train_ds, self.args.teacher_sample_sizes, seeds=TEACHER_SEED, mode=TEACHER)
-            
-            if self.mode==STUDENT: 
-                fewshot_ds = create_fewshot_splits(train_ds, self.args.student_sample_sizes, seeds=STUDENT_SEEDS, mode=STUDENT)   
-                              
+            if self.mode == TEACHER:
+                fewshot_ds = create_fewshot_splits(
+                    train_ds, self.args.teacher_sample_sizes, seeds=TEACHER_SEED, mode=TEACHER
+                )
+
+            if self.mode == STUDENT:
+                fewshot_ds = create_fewshot_splits(
+                    train_ds, self.args.student_sample_sizes, seeds=STUDENT_SEEDS, mode=STUDENT
+                )
+
             for name in fewshot_ds:
                 results_path = os.path.join(self.output_path, dataset, name, "results.json")
                 print(f"\n\n======== {os.path.dirname(results_path)} =======")
                 os.makedirs(os.path.dirname(results_path), exist_ok=True)
                 # if os.path.exists(results_path):
                 #     continue
-              
+
                 self.model.load_state_dict(copy.deepcopy(self.model_wrapper.model_original_state))
                 metrics = self.train_eval_setfit(
                     fewshot_ds[name], test_dataset, self.loss_class, self.args.num_epochs, metric
@@ -214,7 +224,6 @@ class RunFewShot:
 
                 with open(results_path, "w") as f_out:
                     json.dump({"score": metrics[metric] * 100, "measure": metric}, f_out, sort_keys=True)
-                    
 
     def train_eval_setfit(self, train_data, test_data, loss_class, num_epochs, metric):
         x_train = train_data["text"]
@@ -228,31 +237,35 @@ class RunFewShot:
 
         # sentence-transformers adaptation
         batch_size = self.args.batch_size
-    
-        if self.mode==TEACHER:
+
+        if self.mode == TEACHER:
             # save teacher train data for student training
-            self.x_train_teacher = x_train            
+            self.x_train_teacher = x_train
             train_examples = []
             for _ in range(num_epochs):
                 train_examples = sentence_pairs_generation(np.array(x_train), np.array(y_train), train_examples)
 
-        if self.mode==STUDENT:
+        if self.mode == STUDENT:
             # generate student data
             # student train data = teacher train data + unlabeled data
-            x_train = self.x_train_teacher+x_train
+            x_train = self.x_train_teacher + x_train
             x_train_embd_student = self.trained_teacher_model.sbert_model.encode(x_train)
-            y_train                   = self.trained_teacher_model.clf.predict(x_train_embd_student)
-            y_train_pred_student_prob = self.trained_teacher_model.clf.predict_proba(x_train_embd_student)
-           
+            y_train = self.trained_teacher_model.clf.predict(x_train_embd_student)
+            # y_train_pred_student_prob = self.trained_teacher_model.clf.predict_proba(x_train_embd_student)
+
             cos_sim_matrix = [[0 for j in range(len(x_train))] for i in range(len(x_train))]
             for idxA in range(len(x_train)):
                 for idxB in range(len(x_train)):
-                    cos_sim_matrix[idxA][idxB] = float(cosine_similarity(x_train_embd_student[idxA].reshape(1, -1),x_train_embd_student[idxB].reshape(1, -1)))                           
+                    cos_sim_matrix[idxA][idxB] = float(
+                        cosine_similarity(
+                            x_train_embd_student[idxA].reshape(1, -1), x_train_embd_student[idxB].reshape(1, -1)
+                        )
+                    )
 
-            train_examples = [] 
+            train_examples = []
             for x in range(num_epochs):
                 train_examples = sentence_pairs_generation_cos_sim(np.array(x_train), train_examples, cos_sim_matrix)
-                    
+
         train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=batch_size)
         train_loss = loss_class(self.model)
         train_steps = len(train_dataloader)
@@ -272,24 +285,29 @@ class RunFewShot:
         clf = self.get_classifier(self.model)
         clf.fit(x_train, y_train)
         y_pred = clf.predict(x_test)
-        
+
         if self.mode == TEACHER:
             self.full_setfit_model = clf
-        
+
         return self.compute_metrics(x_train, y_train, x_test, y_test, y_pred, metric)
 
 
 def main():
     args = parse_args()
-    
-    # Train few-shot teacher 
-    fewshot_teacher = RunFewShot(args,mode=TEACHER, trained_teacher_model=None, x_train_teacher=None)
+
+    # Train few-shot teacher
+    fewshot_teacher = RunFewShot(args, mode=TEACHER, trained_teacher_model=None, x_train_teacher=None)
     fewshot_teacher.train()
-    
-    # Train few-shot student 
-    fewshot_student = RunFewShot(args,mode=STUDENT, trained_teacher_model=fewshot_teacher.full_setfit_model, x_train_teacher=fewshot_teacher.x_train_teacher)
+
+    # Train few-shot student
+    fewshot_student = RunFewShot(
+        args,
+        mode=STUDENT,
+        trained_teacher_model=fewshot_teacher.full_setfit_model,
+        x_train_teacher=fewshot_teacher.x_train_teacher,
+    )
     fewshot_student.train()
-    
+
 
 if __name__ == "__main__":
     main()
