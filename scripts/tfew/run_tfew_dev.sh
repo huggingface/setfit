556f9145ac57300ea85a6502ffea19f0a464a2ac
Daniel Korat <daniel.korat@intel.com>
2022-07-26 10:19:03 +0300

    Add split number to T-Few experiment names; remove t-few gitignore;
    * t-few fixes
    
    * merge dev.sh changes
    
    * remove gitignore for t-few

--- scripts/tfew/run_tfew_dev.sh
+++ scripts/tfew/run_tfew_dev.sh
@@ -8,7 +8,7 @@ do
             do
                 python -m src.pl_train -c t011b.json+ia3.json+${dataset}.json \
                 -k load_weight="t-few/pretrained_checkpoints/t011b_ia3_finish.pt" \
-                exp_name=t011b_pretrained_${dataset}_${sample_size}_seed${seed} \
+                exp_name=t011b_pretrained_${dataset}/train-${sample_size}-${split}_seed${seed} \
                 train_split=${train_split} \
                 few_shot_random_seed=${seed} \
                 seed=${seed} \
