b7745303de7ff5b90384031d01f03491a6ecb71c
Daniel Korat <daniel.korat@intel.com>
2022-09-01 05:41:46 -0700

    tidy up results folder; ammend tfew test scripts

--- scripts/tfew/run_tfew_debug.sh
+++ scripts/tfew/run_tfew_debug.sh
@@ -1,24 +1,22 @@
-for dataset in sst5
+for dataset in senteval_cr
 do
-    for train_split in 0 
+    for sample_size in 8
     do
-        for seed in 0
+        for train_split in 0 1 2
         do
-            for sample_size in 8
+            for seed in 0 1 2
             do
-                python -m src.pl_train -c t03b.json+ia3.json+${dataset}.json \
-                -k load_weight="t-few/pretrained_checkpoints/t03b_ia3_finish.pt" \
-                exp_name=t03b_pretrained/${dataset}/train-${sample_size}-${train_split}/seed${seed} \
-                train_split=${train_split} \
-                few_shot_random_seed=${seed} \
-                seed=${seed} \
-                num_shot=$sample_size \
-                batch_size=8 \
-                eval_batch_size=2 \
-                grad_accum_factor=1 \
-                num_steps=1 \
-                eval_before_training=0 \
-                allow_skip_exp=0
+                    python -m src.pl_train -c t03b.json+ia3.json+${dataset}.json \
+                    -k load_weight="t-few/pretrained_checkpoints/t03b_ia3_finish.pt" \
+                    exp_name=t03b_pretrained/${dataset}/train-${sample_size}-${train_split}/seed${seed} \
+                    train_split=${train_split} \
+                    few_shot_random_seed=${seed} \
+                    seed=${seed} \
+                    num_shot=$sample_size \
+                    batch_size=8 \
+                    eval_batch_size=16 \
+                    grad_accum_factor=1 \
+                    eval_before_training=0
             done
         done
     done
