06d70302f9aa40ec20ef7a8ff7723391023e4617
Michele Pangrazzi <michele@wonderflow.ai>
2022-10-01 17:51:39 +0200

    Refactor move SetFitModel tests into test_modeling.py

--- tests/test_modeling.py
+++ tests/test_modeling.py
@@ -1,5 +1,10 @@
 import numpy as np
+from sentence_transformers import SentenceTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.multioutput import ClassifierChain, MultiOutputClassifier
 
+from setfit import SetFitModel
 from setfit.modeling import sentence_pairs_generation, sentence_pairs_generation_multilabel
 
 
@@ -31,3 +36,39 @@ def test_sentence_pairs_generation_multilabel():
     assert len(pairs) == 12
     assert pairs[0].texts == ["sent 1", "sent 1"]
     assert pairs[0].label == 1.0
+
+
+def test_setfit_model_body():
+    model = SetFitModel.from_pretrained("sentence-transformers/paraphrase-mpnet-base-v2")
+
+    assert type(model.model_body) is SentenceTransformer
+
+
+def test_setfit_default_model_head():
+    model = SetFitModel.from_pretrained("sentence-transformers/paraphrase-mpnet-base-v2")
+
+    assert type(model.model_head) is LogisticRegression
+
+
+def test_setfit_multilabel_one_vs_rest_model_head():
+    model = SetFitModel.from_pretrained(
+        "sentence-transformers/paraphrase-mpnet-base-v2", multi_target_strategy="one-vs-rest"
+    )
+
+    assert type(model.model_head) is OneVsRestClassifier
+
+
+def test_setfit_multilabel_multi_output_classifier_model_head():
+    model = SetFitModel.from_pretrained(
+        "sentence-transformers/paraphrase-mpnet-base-v2", multi_target_strategy="multi-output"
+    )
+
+    assert type(model.model_head) is MultiOutputClassifier
+
+
+def test_setfit_multilabel_classifier_chain_classifier_model_head():
+    model = SetFitModel.from_pretrained(
+        "sentence-transformers/paraphrase-mpnet-base-v2", multi_target_strategy="classifier-chain"
+    )
+
+    assert type(model.model_head) is ClassifierChain
