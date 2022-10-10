9dc3f7ff091ff21eaa051750e05927713dd6d480
Michele Pangrazzi <michele@wonderflow.ai>
2022-10-10 15:16:10 +0200

    Fix test

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -63,36 +63,44 @@ class SetFitTrainerTest(TestCase):
         with pytest.raises(ValueError):
             trainer.train()
 
+    def test_trainer_raises_error_with_missing_text(self):
+        dataset = Dataset.from_dict({"label": [0, 1, 2], "extra_column": ["d", "e", "f"]})
+        trainer = SetFitTrainer(
+            model=self.model, train_dataset=dataset, eval_dataset=dataset, num_iterations=self.num_iterations
+        )
+        with pytest.raises(ValueError):
+            trainer.train()
 
-def test_column_mapping_with_missing_text(setup_trainer):
-    trainer = setup_trainer
-    trainer.column_mapping = {"label_new": "label"}
-    with pytest.raises(ValueError):
+    def test_column_mapping_with_missing_text(self):
+        dataset = Dataset.from_dict({"text": ["a", "b", "c"], "extra_column": ["d", "e", "f"]})
+        trainer = SetFitTrainer(
+            model=self.model,
+            train_dataset=dataset,
+            eval_dataset=dataset,
+            num_iterations=self.num_iterations,
+            column_mapping={"label_new": "label"},
+        )
+        with pytest.raises(ValueError):
+            trainer._validate_column_mapping(trainer.train_dataset)
+
+    def test_column_mapping_multilabel(self):
+        dataset = Dataset.from_dict({"text_new": ["a", "b", "c"], "label_new": [[0, 1], [1, 2], [2, 0]]})
+
+        trainer = SetFitTrainer(
+            model=self.model,
+            train_dataset=dataset,
+            eval_dataset=dataset,
+            num_iterations=self.num_iterations,
+            column_mapping={"text_new": "text", "label_new": "label"},
+        )
+
         trainer._validate_column_mapping(trainer.train_dataset)
+        formatted_dataset = trainer._apply_column_mapping(trainer.train_dataset, trainer.column_mapping)
 
+        assert formatted_dataset.column_names == ["text", "label"]
 
-def test_column_mapping_multilabel(setup_trainer):
-    trainer = setup_trainer
+        assert formatted_dataset[0]["text"] == "a"
+        assert formatted_dataset[0]["label"] == [0, 1]
 
-    trainer.column_mapping = {"text_new": "text", "label_new": "label"}
-    trainer.train_dataset = Dataset.from_dict({"text_new": ["a", "b", "c"], "label_new": [[0, 1], [1, 2], [2, 0]]})
-
-    trainer._validate_column_mapping(trainer.train_dataset)
-    formatted_dataset = trainer._apply_column_mapping(trainer.train_dataset, trainer.column_mapping)
-
-    assert formatted_dataset.column_names == ["text", "label"]
-
-    assert formatted_dataset[0]["text"] == "a"
-    assert formatted_dataset[0]["label"] == [0, 1]
-
-    assert formatted_dataset[1]["text"] == "b"
-    assert formatted_dataset[1]["label"] == [1, 2]
-
-
-def test_trainer_raises_error_with_missing_text(self):
-    dataset = Dataset.from_dict({"label": [0, 1, 2], "extra_column": ["d", "e", "f"]})
-    trainer = SetFitTrainer(
-        model=self.model, train_dataset=dataset, eval_dataset=dataset, num_iterations=self.num_iterations
-    )
-    with pytest.raises(ValueError):
-        trainer.train()
+        assert formatted_dataset[1]["text"] == "b"
+        assert formatted_dataset[1]["label"] == [1, 2]
