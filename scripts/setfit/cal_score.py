92a8cdcb34cae07bb6f00347728502d11348b239
blakechi <pwchi@ucdavis.edu>
2022-10-29 21:09:21 -0500

    add a flag: keep_body_frozen to control whether to train SetFitModel end to end

--- scripts/setfit/cal_score.py
+++ scripts/setfit/cal_score.py
@@ -1,7 +1,7 @@
-from os import listdir
-from os.path import isfile, isdir, join
 import argparse
 import json
+from os import listdir
+from os.path import isdir, isfile, join
 
 import numpy as np
 
@@ -9,10 +9,11 @@ import numpy as np
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--exp_folder", "-e",
+        "--exp_folder",
+        "-e",
         required=True,
         type=str,
-        help="The folder path of the experiment created by `run_fewshot.py`."
+        help="The folder path of the experiment created by `run_fewshot.py`.",
     )
 
     args = parser.parse_args()
@@ -25,13 +26,13 @@ def get_folders(folder):
 
 
 if __name__ == "__main__":
-    
+
     args = parse_args()
 
     dataset_folders = get_folders(args.exp_folder)
     for dataset_folder in dataset_folders:
         run_folders = get_folders(dataset_folder)
-        
+
         scores = []
         for run_folder in run_folders:
             with open(join(run_folder, "results.json"), "r") as f:
@@ -40,8 +41,10 @@ if __name__ == "__main__":
 
         scores = np.array(scores)
         with open(join(dataset_folder, "results.json"), "w") as f:
-            json.dump({
-                "mean": np.mean(scores).item(),
-                "std": np.std(scores).item(),
-            }, f)
-
+            json.dump(
+                {
+                    "mean": np.mean(scores).item(),
+                    "std": np.std(scores).item(),
+                },
+                f,
+            )
--- scripts/setfit/run_fewshot.py
+++ scripts/setfit/run_fewshot.py
@@ -51,6 +51,7 @@ def parse_args():
     parser.add_argument("--is_dev_set", type=bool, default=False)
     parser.add_argument("--is_test_set", type=bool, default=False)
     parser.add_argument("--override_results", default=False, action="store_true")
+    parser.add_argument("--keep_body_frozen", default=False, action="store_true")
     parser.add_argument("--add_data_augmentation", default=False)
 
     args = parser.parse_args()
@@ -131,13 +132,13 @@ def main():
             if args.classifier == "pytorch":
                 trainer.freeze()
                 trainer.train()
-                trainer.unfreeze(keep_body_frozen=True)
+                trainer.unfreeze(keep_body_frozen=args.keep_body_frozen)
                 trainer.train(
-                    num_epochs=50,
-                    body_learning_rate=5e-4,
-                    learning_rate=1e-2,
-                    l2_weight=0.,
-                    batch_size=16,
+                    num_epochs=25,
+                    body_learning_rate=1e-5,
+                    learning_rate=args.lr,  # recommand: 1e-2
+                    l2_weight=0.0,
+                    batch_size=args.batch_size,
                 )
             else:
                 trainer.train()
