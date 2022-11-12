ed86b2e1c7b2da3c22b43436bd84ca6737999721
nicolas bertagnolli <nmbertagnolli@gmail.com>
2022-11-12 00:44:11 -0700

    Addressed comments, cleaned up code, added comments, minor refactors.

--- src/setfit/onnx.py
+++ src/setfit/onnx.py
@@ -1,49 +1,138 @@
-from .modeling import SetFitModel
 from sentence_transformers import models
 import numpy as np
 import onnx
 import warnings
-from typing import Optional, Union
+from typing import Callable, Optional, Union
 import torch
 from sklearn.linear_model import LogisticRegression
+from transformers.modeling_utils import PreTrainedModel
+import torch
+from torch import nn
+from sentence_transformers import SentenceTransformer
 
 
-class ONNXSetFitModel(torch.nn.Module):
+def mean_pooling(token_embeddings: torch.Tensor, attention_mask: torch.Tensor) -> torch.Tensor:
+    """Perform attention aware mean pooling.
+
+    This method takes in embeddings of shape (batch, sequence, embedding_size) and performs average
+    pooling across the sequence dimension to yield embeddings of size (batch, embedding_size).
+
+    From:
+    https://huggingface.co/sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2#usage-huggingface-transformers
+
+    Args:
+        token_embeddings (`torch.Tensor`): The embeddings we wish to pool over of shape
+            (batch, sequence, embedding_size).  This will pool over the sequence to yield
+            (batch, embedding_size).
+        attention_mask (`torch.Tensor`): The binary attention mask across the embedings of shape
+
+    Returns:
+        (`torch.Tensor`) The mean pooled embeddings of size (batch, embedding_size).
+    """
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+
+class OnnxSetFitModel(torch.nn.Module):
+    """A wrapper around SetFit model body, pooler, and model head which makes ONNX exporting easier.
+
+    This wrapper allows us to create a nn.Module with different levels of connectivity. We can set
+    model_body and pooler and have a Module which maps inputs to embeddings or we can set all three
+    and have a model which maps inputs to final predictions. This is useful because torch.onnx.export
+    will work with a nn.Module.
+
+    Attributes:
+        model_body (`PreTrainedModel`): The pretrained model body of a setfit model.
+        pooler (`nn.Module` or `Callable[[torch.Tensor], torch.Tensor]`): The callable function that can
+            map  tensors of shape (batch, sequence, embedding_dim) to shape (batch, embedding_dim).
+        model_head: (`torch.nn.Module` or `LogisticRegression`): The model head from the pretrained SetFit model.
+            if it is None then the resulting OnnxSetFitModel forward will return embeddings instead of predictions.
+    """
+
     def __init__(
         self,
-        model_body,
-        pooler,
+        model_body: PreTrainedModel,
+        pooler: Optional[Union[nn.Module, Callable[[torch.Tensor], torch.Tensor]]] = None,
         model_head: Optional[Union[torch.nn.Module, LogisticRegression]] = None,
     ):
         super().__init__()
 
         self.model_body = model_body
-        self.pooler = pooler
+        if pooler is None:
+            self.pooler = mean_pooling
+        else:
+            self.pooler = pooler
         self.model_head = model_head
 
-    def forward(self, input_ids, attention_mask, token_type_ids):
-        hidden = self.model_body(input_ids, attention_mask, token_type_ids)
-        hidden = {"token_embeddings": hidden[0], "attention_mask": attention_mask}
+    def forward(self, input_ids: torch.Tensor, attention_mask: torch.Tensor, token_type_ids: torch.Tensor):
+        hidden_states = self.model_body(input_ids, attention_mask, token_type_ids)
+        hidden_states = {"token_embeddings": hidden_states[0], "attention_mask": attention_mask}
 
-        hidden = self.pooler(hidden)["sentence_embedding"]
+        embeddings = self.pooler(hidden_states)
+
+        # If the model_head is none we are using a sklearn head and only output
+        # the embeddings from the setfit model
         if self.model_head is None:
-            return hidden
+            return embeddings
 
-        out = self.model_head(hidden)
+        # If head is set then we have a fully torch based model and make the final predictions
+        # with the head.
+        out = self.model_head(embeddings)
         return out
 
 
-def sklearn_head_to_onnx(model_head: LogisticRegression, opset: int):
-    """
-    Convert the sklearn head from a SetFitModel to ONNX format.
+def export_onnx_setfit_model(setfit_model: OnnxSetFitModel, inputs, output_path, opset):
+    """Export the OnnxSetFitModel.
 
-    :param model_head: The trained SetFit model_head.
-    :param opset: The ONNX opset to use for optimizing this model. The opset is not
-        gauranteed and will default to the maximum version possible for the sklearn
-        model.
-    :raises ImportError: If skl2onnx is not installed an error will be raised asking
-        to install this package.
-    :return: The ONNX model generated from the sklearn head.
+    This exports the model created by the OnnxSetFitModel wrapper using torch.onnx.export.
+
+    Args:
+        setfit_model (`OnnxSetFitModel`): The OnnxSetFitModel we want to export to .onnx format.
+        inputs (`Dict[str, torch.Tensor]`): The inputs we would hypothetically pass to the model. These are
+            generated using a tokenizer.
+        output_path (`str`): The local path to save the onnx model to.
+        opset (`int`): The ONNX opset to use for the export.
+    """
+    input_names = list(inputs.keys())
+    output_names = ["logits"]
+
+    # Setup the dynamic axes for onnx conversion.
+    dynamic_axes_input = {}
+    for input_name in input_names:
+        dynamic_axes_input[input_name] = {0: "batch_size", 1: "sequence"}
+
+    dynamic_axes_output = {}
+    for output_name in output_names:
+        dynamic_axes_output[output_name] = {0: "batch_size"}
+
+    setfit_model.eval()
+    with torch.no_grad():
+        torch.onnx.export(
+            setfit_model,
+            args=tuple(inputs.values()),
+            f=output_path,
+            opset_version=opset,
+            input_names=["input_ids", "attention_mask", "token_type_ids"],
+            output_names=output_names,
+            dynamic_axes={**dynamic_axes_input, **dynamic_axes_output},
+        )
+
+
+def export_sklearn_head_to_onnx(model_head: LogisticRegression, opset: int) -> onnx.onnx_ml_pb2.ModelProto:
+    """Convert the Scikit-Learn head from a SetFitModel to ONNX format.
+
+    Args:
+        model_head (`LogisticRegression`): The trained SetFit model_head.
+        opset (`int`): The ONNX opset to use for optimizing this model. The opset is not
+            gauranteed and will default to the maximum version possible for the sklearn
+            model.
+
+    Returns:
+        [`onnx.onnx_ml_pb2.ModelProto`] The ONNX model generated from the sklearn head.
+
+    Raises:
+        ImportError: If skl2onnx is not installed an error will be raised asking
+            to install this package.
     """
 
     # Check if skl2onnx is installed
@@ -54,9 +143,10 @@ def sklearn_head_to_onnx(model_head: LogisticRegression, opset: int):
         from skl2onnx.sklapi import CastTransformer
         from sklearn.pipeline import Pipeline
     except ImportError:
-        msg = (
-            "skl2onnx must be installed in order to convert a model with an sklearn head."
-        )
+        msg = """
+        skl2onnx must be installed in order to convert a model with an sklearn head.
+        Please install with pip install skl2onnx.
+        """
         raise ImportError(msg)
 
     # Check to see that the head has a coef_
@@ -73,60 +163,54 @@ def sklearn_head_to_onnx(model_head: LogisticRegression, opset: int):
     # If the datatype of the model is double we need to cast the outputs
     # from the setfit model to doubles for compatibility inside of ONNX.
     if type(dtype) == onnxconverter_common.data_types.DoubleTensorType:
-        # TODO:: TALK ABOUT FLOAT CONVERSION ISSUES.
-        sk_model = Pipeline(
-            [("castdouble", CastTransformer(dtype=np.double)), ("head", model_head)]
-        )
+        sklearn_model = Pipeline([("castdouble", CastTransformer(dtype=np.double)), ("head", model_head)])
     else:
-        sk_model = model_head
+        sklearn_model = model_head
 
     # Convert sklearn head into ONNX format
-    initial_type = [("model_head", dtype)]
-    onx = convert_sklearn(
-        sk_model,
-        initial_types=initial_type,
+    onnx_model = convert_sklearn(
+        sklearn_model,
+        initial_types=[("model_head", dtype)],
         target_opset=opset,
-        options={id(sk_model): {"zipmap": False}},
+        options={id(sklearn_model): {"zipmap": False}},
     )
 
-    return onx
+    return onnx_model
 
 
 def export_onnx(
-    model_body_sentence_transformer,
-    model_head,
+    model_body: SentenceTransformer,
+    model_head: Union[torch.nn.Module, LogisticRegression],
     opset: int,
-    output: str,
+    output_path: str,
     ignore_ir_version: bool = True,
-):
-    """
-    Export a PyTorch backed setfit model to ONNX Intermediate Representation.
+) -> None:
+    """Export a PyTorch backed SetFit model to ONNX Intermediate Representation.
 
     Args:
-        model_body_sentence_transformer: The model_body from a SetFitModel.model_body. This should be a
+        model_body (`SentenceTransformer`): The model_body from a SetFit model body. This should be a
             SentenceTransformer.
-        model_head: The model_head from SetFitModel.model_head. This can be either a dense
-            layer SetFitHead or a Sklearn estimator.
-        opset: The actual version of the ONNX operator set to use.  The final opset used
-            might be lower. ONNX will use the highest version supported by both the
-            sklearn head and the model body. If versions can't be rectified an error
-            will be thrown.
-        output: The path where will be stored the generated ONNX model. If the head is an
-            sklearn.estimator the models will be saved in
-        ignore_ir_version: Whether to ignore the IR version used in sklearn. The version is often missmatched
+        model_head (`torch.nn.Module` or `LogisticRegression`): The SetFit model head. This can be either a
+            dense layer SetFitHead or a Sklearn estimator.
+        opset (`int`): The actual version of the ONNX operator set to use.  The final opset used might be lower.
+            ONNX will use the highest version supported by both the sklearn head and the model body. If versions
+            can't be rectified an error will be thrown.
+        output_path (`str`): The path where will be stored the generated ONNX model. If the head is an sklearn.estimator the
+            models will be saved in
+        ignore_ir_version (`bool`): Whether to ignore the IR version used in sklearn. The version is often missmatched
             with the transformer models. Setting this to true coerces the versions to be the same. This might
             cause errors but in practice works.  If this is set to False you need to ensure that the IR versions
             align between the transformer and the sklearn onnx representation.
-    Returns:
     """
 
     # Load the model and get all of the parts.
-    transformer = model_body_sentence_transformer._modules["0"]
-    tokenizer = transformer.tokenizer
-    max_length = transformer.max_seq_length
-    model_body = transformer.auto_model
-    model_pooler = model_body_sentence_transformer._modules["1"]
-    model_body.eval()
+    model_body_module = model_body._modules["0"]
+    pooling_layer = model_body._modules["1"]
+    tokenizer = model_body_module.tokenizer
+    max_length = model_body_module.max_seq_length
+    transformer = model_body_module.auto_model
+    model_pooler = lambda x: pooling_layer(x)["sentence_embedding"]
+    transformer.eval()
 
     # Create dummy data to use during onnx export.
     tokenizer_kwargs = dict(
@@ -142,36 +226,19 @@ def export_onnx(
 
     # Check to see if the model uses a sklearn head or a torch dense layer.
     if issubclass(type(model_head), models.Dense):
-        setfit_model = ONNXSetFitModel(model_body, model_pooler, model_head).cpu()
-        setfit_model.eval()
-        with torch.no_grad():
-            torch.onnx.export(
-                setfit_model,
-                args=tuple(dummy_inputs.values()),
-                f=output,
-                opset_version=opset,
-                input_names=["input_ids", "attention_mask", "token_type_ids"],
-                output_names=["prediction"],
-                dynamic_axes={
-                    "input_ids": symbolic_names,  # variable length axes
-                    "attention_mask": symbolic_names,
-                    "token_type_ids": symbolic_names,
-                    "prediction": {0: "batch_size"},
-                },
-            )
+        setfit_model = OnnxSetFitModel(transformer, model_pooler, model_head).cpu()
+        export_onnx_setfit_model(setfit_model, dummy_inputs, output_path, opset)
 
         # store meta data of the tokenizer for getting the correct tokenizer during inference
-        onnx_setfit_model = onnx.load(output)
+        onnx_setfit_model = onnx.load(output_path)
         meta = onnx_setfit_model.metadata_props.add()
         meta.key = "model_name"
-        meta.value = model_name
+        meta.value = transformer.name_or_path
         for key, value in tokenizer_kwargs.items():
-            meta = (
-                onnx_setfit_model.metadata_props.add()
-            )  # create a new key-value pair to store
+            meta = onnx_setfit_model.metadata_props.add()  # create a new key-value pair to store
             meta.key = key
             meta.value = value
-        onnx.save(onnx_setfit_model, output)
+
     else:
         # TODO:: Make this work for other sklearn models without coef_.
         if not hasattr(model_head, "coef_"):
@@ -179,32 +246,16 @@ def export_onnx(
 
         # Export the sklearn head first to get the minimum opset.  sklearn is behind
         # in supported opsets.
-        onnx_head = sklearn_head_to_onnx(model_head, opset)
+        onnx_head = export_sklearn_head_to_onnx(model_head, opset)
         max_opset = onnx_head.opset_import[0].version
 
         if max_opset != opset:
             warnings.warn(
                 f"sklearn onnx max opset is {max_opset} requested opset {opset} using opset {max_opset} for compatibility."
             )
+        export_onnx_setfit_model(OnnxSetFitModel(transformer, model_pooler), dummy_inputs, output_path, max_opset)
 
-        model_body.eval()
-        with torch.no_grad():
-            torch.onnx.export(
-                ONNXSetFitModel(model_body, model_pooler),
-                args=tuple(dummy_inputs.values()),
-                f=output,
-                opset_version=max_opset,
-                input_names=["input_ids", "attention_mask", "token_type_ids"],
-                output_names=["prediction"],
-                dynamic_axes={
-                    "input_ids": symbolic_names,  # variable length axes
-                    "attention_mask": symbolic_names,
-                    "token_type_ids": symbolic_names,
-                    "prediction": {0: "batch_size"},
-                },
-            )
-
-        onnx_body = onnx.load(output)
+        onnx_body = onnx.load(output_path)
 
         # Check that the ir_versions are aligned and if not align them.
         if ignore_ir_version:
@@ -218,10 +269,11 @@ def export_onnx(
             raise ValueError(msg)
 
         # Combine the onnx body and head by mapping the pooled output to the input of the sklearn model.
-        combined_model = onnx.compose.merge_models(
+        onnx_setfit_model = onnx.compose.merge_models(
             onnx_body,
             onnx_head,
-            io_map=[("prediction", "model_head")],
+            io_map=[("logits", "model_head")],
         )
 
-        onnx.save(combined_model, output)
+    # Save the final model.
+    onnx.save(onnx_setfit_model, output_path)
