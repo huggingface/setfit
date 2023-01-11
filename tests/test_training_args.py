fd68274ade7066a5317ba47dd8f3d1b892dfd117
Tom Aarsen <Cubiegamedev@gmail.com>
2023-01-11 18:33:59 +0100

    Add TrainingArgument tests for num_epochs, batch_sizes, lr

--- tests/test_training_args.py
+++ tests/test_training_args.py
@@ -14,3 +14,78 @@ class TestTrainingArguments(TestCase):
         # warmup_proportion must not be < 0.0
         with pytest.raises(ValueError):
             TrainingArguments(warmup_proportion=-0.1)
+
+    def test_training_args_batch_sizes(self):
+        batch_size_A = 12
+        batch_size_B = 4
+        batch_size_C = 6
+
+        args = TrainingArguments(batch_size=batch_size_A)
+        assert args.batch_size == (batch_size_A, batch_size_A)
+        assert args.embedding_batch_size == batch_size_A
+        assert args.classifier_batch_size == batch_size_A
+
+        args = TrainingArguments(batch_size=(batch_size_A, batch_size_B))
+        assert args.batch_size == (batch_size_A, batch_size_B)
+        assert args.embedding_batch_size == batch_size_A
+        assert args.classifier_batch_size == batch_size_B
+
+        args = TrainingArguments(batch_size=(batch_size_A, batch_size_B), embedding_batch_size=batch_size_C)
+        assert args.batch_size == (batch_size_A, batch_size_B)
+        assert args.embedding_batch_size == batch_size_C
+        assert args.classifier_batch_size == batch_size_B
+
+        args = TrainingArguments(batch_size=batch_size_A, embedding_batch_size=batch_size_C)
+        assert args.batch_size == (batch_size_A, batch_size_A)
+        assert args.embedding_batch_size == batch_size_C
+        assert args.classifier_batch_size == batch_size_A
+
+    def test_training_args_num_epochs(self):
+        num_epochs_A = 12
+        num_epochs_B = 4
+        num_epochs_C = 6
+
+        args = TrainingArguments(num_epochs=num_epochs_A)
+        assert args.num_epochs == (num_epochs_A, num_epochs_A)
+        assert args.embedding_num_epochs == num_epochs_A
+        assert args.classifier_num_epochs == num_epochs_A
+
+        args = TrainingArguments(num_epochs=(num_epochs_A, num_epochs_B))
+        assert args.num_epochs == (num_epochs_A, num_epochs_B)
+        assert args.embedding_num_epochs == num_epochs_A
+        assert args.classifier_num_epochs == num_epochs_B
+
+        args = TrainingArguments(num_epochs=(num_epochs_A, num_epochs_B), embedding_num_epochs=num_epochs_C)
+        assert args.num_epochs == (num_epochs_A, num_epochs_B)
+        assert args.embedding_num_epochs == num_epochs_C
+        assert args.classifier_num_epochs == num_epochs_B
+
+        args = TrainingArguments(num_epochs=num_epochs_A, embedding_num_epochs=num_epochs_C)
+        assert args.num_epochs == (num_epochs_A, num_epochs_A)
+        assert args.embedding_num_epochs == num_epochs_C
+        assert args.classifier_num_epochs == num_epochs_A
+
+    def test_training_args_learning_rates(self):
+        learning_rate_A = 1e-2
+        learning_rate_B = 1e-3
+        learning_rate_C = 1e-4
+
+        base = TrainingArguments()
+
+        args = TrainingArguments(classifier_learning_rate=learning_rate_A)
+        assert args.classifier_learning_rate == (base.embedding_learning_rate, learning_rate_A)
+        assert args.embedding_learning_rate == base.embedding_learning_rate
+
+        args = TrainingArguments(classifier_learning_rate=learning_rate_A, embedding_learning_rate=learning_rate_B)
+        assert args.classifier_learning_rate == (learning_rate_B, learning_rate_A)
+        assert args.embedding_learning_rate == learning_rate_B
+
+        args = TrainingArguments(
+            classifier_learning_rate=(learning_rate_C, learning_rate_A), embedding_learning_rate=learning_rate_B
+        )
+        assert args.classifier_learning_rate == (learning_rate_C, learning_rate_A)
+        assert args.embedding_learning_rate == learning_rate_B
+
+        args = TrainingArguments(classifier_learning_rate=(learning_rate_C, learning_rate_A))
+        assert args.classifier_learning_rate == (learning_rate_C, learning_rate_A)
+        assert args.embedding_learning_rate == base.embedding_learning_rate
