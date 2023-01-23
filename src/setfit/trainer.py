721b3841e57c23f99323fb792405613549b9854b
Thomas Werkmeister <thomas@werkmeister.me>
2023-01-23 09:50:29 +0100

    reinstated total_train_step variable

--- src/setfit/trainer.py
+++ src/setfit/trainer.py
@@ -361,14 +361,15 @@ class SetFitTrainer:
 
                 train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=batch_size)
                 train_loss = self.loss_class(self.model.model_body)
-
+      
+            total_train_steps = len(train_dataloader) * num_epochs
             logger.info("***** Running training *****")
             logger.info(f"  Num examples = {len(train_examples)}")
             logger.info(f"  Num epochs = {num_epochs}")
-            logger.info(f"  Total optimization steps = {len(train_dataloader) * num_epochs}")
+            logger.info(f"  Total optimization steps = {total_train_steps}")
             logger.info(f"  Total train batch size = {batch_size}")
 
-            warmup_steps = math.ceil(train_steps * self.warmup_proportion)
+            warmup_steps = math.ceil(total_train_steps * self.warmup_proportion)
             self.model.model_body.fit(
                 train_objectives=[(train_dataloader, train_loss)],
                 epochs=num_epochs,
