da55732933e3838ea907fb64e07cd17a945a1a70
Tom Aarsen <Cubiegamedev@gmail.com>
2023-01-24 10:22:39 +0100

    Exclude compatibility versions from dev setup

--- setup.py
+++ setup.py
@@ -10,26 +10,17 @@ MAINTAINER = "Lewis Tunstall, Tom Aarsen"
 MAINTAINER_EMAIL = "lewis@huggingface.co"
 
 INTEGRATIONS_REQUIRE = ["optuna"]
-
 REQUIRED_PKGS = ["datasets>=2.3.0", "sentence-transformers>=2.2.1", "evaluate>=0.3.0"]
-
 QUALITY_REQUIRE = ["black", "flake8", "isort", "tabulate"]
-
 ONNX_REQUIRE = ["onnxruntime", "onnx", "skl2onnx"]
-
 OPENVINO_REQUIRE = ["hummingbird-ml", "openvino>=2022.3"]
-
 TESTS_REQUIRE = ["pytest", "pytest-cov"] + ONNX_REQUIRE + OPENVINO_REQUIRE
-
-COMPAT_TESTS_REQUIRE = [requirement.replace(">=", "==") for requirement in REQUIRED_PKGS] + TESTS_REQUIRE
-
 EXTRAS_REQUIRE = {
     "optuna": INTEGRATIONS_REQUIRE,
     "quality": QUALITY_REQUIRE,
     "tests": TESTS_REQUIRE,
     "onnx": ONNX_REQUIRE,
     "openvino": ONNX_REQUIRE + OPENVINO_REQUIRE,
-    "compat_tests": COMPAT_TESTS_REQUIRE,
 }
 
 
@@ -38,7 +29,7 @@ def combine_requirements(base_keys):
 
 
 EXTRAS_REQUIRE["dev"] = combine_requirements([k for k in EXTRAS_REQUIRE])
-
+EXTRAS_REQUIRE["compat_tests"] = [requirement.replace(">=", "==") for requirement in REQUIRED_PKGS] + TESTS_REQUIRE
 
 setup(
     name="setfit",
