645f1b5bddf6c1cc21334bcf5f248739b00bef77
Jørgen Lund <51115698+jaalu@users.noreply.github.com>
2023-02-06 15:56:26 +0100

    Add unit test to check whether warning raised

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -93,6 +93,29 @@ class SetFitTrainerTest(TestCase):
         with pytest.raises(ValueError):
             trainer._validate_column_mapping(trainer.train_dataset)
 
+    def test_trainer_raises_warning_when_dataset_not_split(self):
+        """Verify that a warning is raised if we pass an unsplit dataset to the trainer."""
+        dataset = Dataset.from_dict({"text": ["a", "b", "c", "d"], "label": [0, 0, 1, 1]}).train_test_split(
+            test_size=0.5
+        )
+        trainer = SetFitTrainer(
+            model=self.model, train_dataset=dataset, eval_dataset=dataset, num_iterations=self.num_iterations
+        )
+        with self.assertLogs(level=logging.WARNING) as cm:
+            try:
+                trainer._validate_column_mapping(trainer.train_dataset)
+            except:
+                pass
+            self.assertEqual(
+                cm.output,
+                [
+                    (
+                        f"WARNING: The dataset has columns named 'train' or 'test',",
+                        f"did you want to select the training/test split with dataset['train'] or dataset['test'] ?",
+                    )
+                ],
+            )
+
     def test_column_mapping_multilabel(self):
         dataset = Dataset.from_dict({"text_new": ["a", "b", "c"], "label_new": [[0, 1], [1, 2], [2, 0]]})
 
