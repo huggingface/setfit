78851287535305ef32f789c7a87004628172b5b6
Logan <logan.markewich@live.com>
2023-02-14 11:39:46 -0600

    Improve dataset pre-processing speeds for large datasets (#309)
    * Greatly imrpove sample_dataset speed
    
    * Add pair generation progress bars, improve basic pair generation speed
    
    * fix sample_dataset for unit tests
    
    * Fix formatting
    
    * Clean up imports
    
    * Show overall pair generation progress instead of per-iteration
    
    * Update src/setfit/modeling.py
    
    Co-authored-by: Tom Aarsen <37621491+tomaarsen@users.noreply.github.com>
    
    * Update src/setfit/trainer.py
    
    Co-authored-by: Tom Aarsen <37621491+tomaarsen@users.noreply.github.com>
    
    * Update src/setfit/trainer.py
    
    Co-authored-by: Tom Aarsen <37621491+tomaarsen@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Logan Markewich <Logan.Markewich@yardi.com>
    Co-authored-by: Tom Aarsen <37621491+tomaarsen@users.noreply.github.com>

--- src/setfit/trainer.py
+++ src/setfit/trainer.py
@@ -8,6 +8,7 @@ from sentence_transformers import InputExample, losses
 from sentence_transformers.datasets import SentenceLabelDataset
 from sentence_transformers.losses.BatchHardTripletLoss import BatchHardTripletLossDistanceFunction
 from torch.utils.data import DataLoader
+from tqdm.auto import trange
 from transformers.trainer_utils import HPSearchBackend, default_compute_objective, number_of_arguments, set_seed
 
 from . import logging
@@ -365,7 +366,7 @@ class SetFitTrainer:
             else:
                 train_examples = []
 
-                for _ in range(self.num_iterations):
+                for _ in trange(self.num_iterations, desc="Generating Training Pairs", disable=not show_progress_bar):
                     if self.model.multi_target_strategy is not None:
                         train_examples = sentence_pairs_generation_multilabel(
                             np.array(x_train), np.array(y_train), train_examples
