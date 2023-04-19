339f332e820c4d1f78194abbee4269db19a584d4
Tom Aarsen <Cubiegamedev@gmail.com>
2023-04-19 09:05:35 +0200

    Execute deprecations

--- src/setfit/data.py
+++ src/setfit/data.py
@@ -21,15 +21,6 @@ SEEDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 SAMPLE_SIZES = [2, 4, 8, 16, 32, 64]
 
 
-def get_augmented_samples(*args, **kwargs) -> None:
-    warnings.warn(
-        "`get_augmented_samples` has been deprecated and will be removed in v1.0.0 of SetFit. "
-        "Please use `get_templated_dataset` instead.",
-        DeprecationWarning,
-        stacklevel=2,
-    )
-
-
 def get_templated_dataset(
     dataset: Optional[Dataset] = None,
     candidate_labels: Optional[List[str]] = None,
@@ -113,15 +104,6 @@ def get_templated_dataset(
             dataset = dataset.add_item(example)
 
     return dataset
-
-
-def add_templated_examples(*args, **kwargs) -> None:
-    warnings.warn(
-        "`add_templated_examples` has been deprecated and will be removed in v1.0.0 of SetFit. "
-        "Please use `get_templated_dataset` instead.",
-        DeprecationWarning,
-        stacklevel=2,
-    )
 
 
 def get_candidate_labels(dataset_name: str, label_names_column: str = "label_text") -> List[str]:
