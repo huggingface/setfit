f114572c8bce53bd5b0acab72fd46dc360994e00
Tom Aarsen <Cubiegamedev@gmail.com>
2023-11-15 16:35:06 +0100

    Remove outdated tests

--- tests/test_training_args.py
+++ tests/test_training_args.py
@@ -31,16 +31,6 @@ class TestTrainingArguments(TestCase):
         self.assertEqual(args.embedding_batch_size, batch_size_A)
         self.assertEqual(args.classifier_batch_size, batch_size_B)
 
-        args = TrainingArguments(batch_size=(batch_size_A, batch_size_B), embedding_batch_size=batch_size_C)
-        self.assertEqual(args.batch_size, (batch_size_A, batch_size_B))
-        self.assertEqual(args.embedding_batch_size, batch_size_C)
-        self.assertEqual(args.classifier_batch_size, batch_size_B)
-
-        args = TrainingArguments(batch_size=batch_size_A, embedding_batch_size=batch_size_C)
-        self.assertEqual(args.batch_size, (batch_size_A, batch_size_A))
-        self.assertEqual(args.embedding_batch_size, batch_size_C)
-        self.assertEqual(args.classifier_batch_size, batch_size_A)
-
     def test_num_epochs(self):
         num_epochs_A = 12
         num_epochs_B = 4
@@ -55,16 +45,6 @@ class TestTrainingArguments(TestCase):
         self.assertEqual(args.num_epochs, (num_epochs_A, num_epochs_B))
         self.assertEqual(args.embedding_num_epochs, num_epochs_A)
         self.assertEqual(args.classifier_num_epochs, num_epochs_B)
-
-        args = TrainingArguments(num_epochs=(num_epochs_A, num_epochs_B), embedding_num_epochs=num_epochs_C)
-        self.assertEqual(args.num_epochs, (num_epochs_A, num_epochs_B))
-        self.assertEqual(args.embedding_num_epochs, num_epochs_C)
-        self.assertEqual(args.classifier_num_epochs, num_epochs_B)
-
-        args = TrainingArguments(num_epochs=num_epochs_A, embedding_num_epochs=num_epochs_C)
-        self.assertEqual(args.num_epochs, (num_epochs_A, num_epochs_A))
-        self.assertEqual(args.embedding_num_epochs, num_epochs_C)
-        self.assertEqual(args.classifier_num_epochs, num_epochs_A)
 
     def test_learning_rates(self):
         learning_rate_A = 1e-2
@@ -84,34 +64,6 @@ class TestTrainingArguments(TestCase):
         self.assertEqual(args.body_embedding_learning_rate, learning_rate_A)
         self.assertEqual(args.body_classifier_learning_rate, learning_rate_B)
         self.assertEqual(args.head_learning_rate, base.head_learning_rate)
-
-        args = TrainingArguments(
-            body_learning_rate=(learning_rate_A, learning_rate_B), head_learning_rate=learning_rate_C
-        )
-        self.assertEqual(args.body_learning_rate, (learning_rate_A, learning_rate_B))
-        self.assertEqual(args.body_embedding_learning_rate, learning_rate_A)
-        self.assertEqual(args.body_classifier_learning_rate, learning_rate_B)
-        self.assertEqual(args.head_learning_rate, learning_rate_C)
-
-        args = TrainingArguments(
-            body_learning_rate=learning_rate_A,
-            body_embedding_learning_rate=learning_rate_B,
-            head_learning_rate=learning_rate_C,
-        )
-        # Perhaps not ideal, but body_learning_rate is never used directly:
-        self.assertEqual(args.body_learning_rate, (learning_rate_A, learning_rate_A))
-        self.assertEqual(args.body_embedding_learning_rate, learning_rate_B)
-        self.assertEqual(args.body_classifier_learning_rate, learning_rate_A)
-        self.assertEqual(args.head_learning_rate, learning_rate_C)
-
-        args = TrainingArguments(
-            body_classifier_learning_rate=learning_rate_A,
-            body_embedding_learning_rate=learning_rate_B,
-            head_learning_rate=learning_rate_C,
-        )
-        self.assertEqual(args.body_embedding_learning_rate, learning_rate_B)
-        self.assertEqual(args.body_classifier_learning_rate, learning_rate_A)
-        self.assertEqual(args.head_learning_rate, learning_rate_C)
 
     def test_report_to(self):
         args = TrainingArguments(report_to="none")
