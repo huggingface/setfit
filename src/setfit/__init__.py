85042a2211f053df0fe72c772cf1f76d00fc0379
Tom Aarsen <Cubiegamedev@gmail.com>
2023-11-29 19:51:32 +0100

    Heavily improve automatic model card generation

--- src/setfit/__init__.py
+++ src/setfit/__init__.py
@@ -1,5 +1,7 @@
 __version__ = "1.0.0.dev0"
 
+import importlib
+import os
 import warnings
 
 from .data import get_templated_dataset, sample_dataset
@@ -13,3 +15,8 @@ from .training_args import TrainingArguments
 # Ensure that DeprecationWarnings are shown by default, as recommended by
 # https://docs.python.org/3/library/warnings.html#overriding-the-default-filter
 warnings.filterwarnings("default", category=DeprecationWarning)
+
+# If codecarbon is installed and the log level is not defined,
+# automatically overwrite the default to "error"
+if importlib.util.find_spec("codecarbon") and "CODECARBON_LOG_LEVEL" not in os.environ:
+    os.environ["CODECARBON_LOG_LEVEL"] = "error"
