f4d9f5eeddff8dee6ab240df6231309a2cd0f869
Tom Aarsen <Cubiegamedev@gmail.com>
2023-12-07 16:36:07 +0100

    Release: v1.0.1

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ EXTRAS_REQUIRE["compat_tests"] = (
 
 setup(
     name="setfit",
-    version="1.0.0",
+    version="1.0.1",
     description="Efficient few-shot learning with Sentence Transformers",
     long_description=README_TEXT,
     long_description_content_type="text/markdown",
