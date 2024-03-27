ba7e7705d6fcb91aec8e621dc471df479023bf9d
Wauplin <lucainp@gmail.com>
2024-03-27 10:10:33 +0100

    Fix SetFitModel: not a dataclass, not a PyTorchModelHubMixin

--- src/setfit/modeling.py
+++ src/setfit/modeling.py
@@ -6,7 +6,6 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Dict, List, Optional, Set, Tuple, Union
 
-
 # For Python 3.7 compatibility
 try:
     from typing import Literal
@@ -17,12 +16,11 @@ import joblib
 import numpy as np
 import requests
 import torch
-from huggingface_hub import PyTorchModelHubMixin, hf_hub_download
+from huggingface_hub import ModelHubMixin, hf_hub_download
 from huggingface_hub.utils import validate_hf_hub_args
 from packaging.version import Version, parse
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, models
 from sentence_transformers import __version__ as sentence_transformers_version
-from sentence_transformers import models
 from sklearn.linear_model import LogisticRegression
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.multioutput import ClassifierChain, MultiOutputClassifier
@@ -35,7 +33,6 @@ from . import logging
 from .data import SetFitDataset
 from .model_card import SetFitModelCardData, generate_model_card
 from .utils import set_docstring
-
 
 logging.set_verbosity_info()
 logger = logging.get_logger(__name__)
@@ -196,8 +193,7 @@ class SetFitHead(models.Dense):
         return "SetFitHead({})".format(self.get_config_dict())
 
 
-@dataclass
-class SetFitModel(PyTorchModelHubMixin):
+class SetFitModel(ModelHubMixin):
     """A SetFit model with integration to the [Hugging Face Hub](https://huggingface.co).
 
     Example::
