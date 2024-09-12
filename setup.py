fc36049cecdc7e5285f9f74747eea48e550c5423
Tom Aarsen <Cubiegamedev@gmail.com>
2024-09-12 12:29:16 +0200

    Avoid onnx==1.16.2 due to errors on Windows

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ REQUIRED_PKGS = [
 ]
 ABSA_REQUIRE = ["spacy"]
 QUALITY_REQUIRE = ["black", "flake8", "isort", "tabulate"]
-ONNX_REQUIRE = ["onnxruntime", "onnx", "skl2onnx"]
+ONNX_REQUIRE = ["onnxruntime", "onnx!=1.16.2", "skl2onnx"]
 OPENVINO_REQUIRE = ["hummingbird-ml<0.4.9", "openvino==2022.3.0"]
 TESTS_REQUIRE = ["pytest", "pytest-cov"] + ONNX_REQUIRE + OPENVINO_REQUIRE + ABSA_REQUIRE
 DOCS_REQUIRE = ["hf-doc-builder>=0.3.0"]
