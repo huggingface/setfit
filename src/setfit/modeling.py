3904e53347c49893048aa78b2da276fbecbecb9b
Lucain <lucainp@gmail.com>
2024-09-12 10:52:11 +0200

    Fix SetFitModel: not a dataclass, not a PyTorchModelHubMixin (#505)
    * Fix SetFitModel: not a dataclass, not a PyTorchModelHubMixin
    
    * adapt SpanSetFitModel and PolarityModel
    
    * super
    
    * fix tests ?
    
    * fix tests
    
    * Apply formatting
    
    ---------
    
    Co-authored-by: Tom Aarsen <Cubiegamedev@gmail.com>

--- src/setfit/modeling.py
+++ src/setfit/modeling.py
@@ -2,7 +2,6 @@ import json
 import os
 import tempfile
 import warnings
-from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Dict, List, Literal, Optional, Set, Tuple, Union
 
@@ -10,7 +9,7 @@ import joblib
 import numpy as np
 import requests
 import torch
-from huggingface_hub import PyTorchModelHubMixin, hf_hub_download
+from huggingface_hub import ModelHubMixin, hf_hub_download
 from huggingface_hub.utils import validate_hf_hub_args
 from packaging.version import Version, parse
 from sentence_transformers import SentenceTransformer
@@ -189,8 +188,7 @@ class SetFitHead(models.Dense):
         return "SetFitHead({})".format(self.get_config_dict())
 
 
-@dataclass
-class SetFitModel(PyTorchModelHubMixin):
+class SetFitModel(ModelHubMixin):
     """A SetFit model with integration to the [Hugging Face Hub](https://huggingface.co).
 
     Example::
@@ -205,19 +203,27 @@ class SetFitModel(PyTorchModelHubMixin):
         ['positive', 'negative', 'negative']
     """
 
-    model_body: Optional[SentenceTransformer] = None
-    model_head: Optional[Union[SetFitHead, LogisticRegression]] = None
-    multi_target_strategy: Optional[str] = None
-    normalize_embeddings: bool = False
-    labels: Optional[List[str]] = None
-    model_card_data: Optional[SetFitModelCardData] = field(default_factory=SetFitModelCardData)
-    sentence_transformers_kwargs: Dict = field(default_factory=dict, repr=False)
+    def __init__(
+        self,
+        model_body: Optional[SentenceTransformer] = None,
+        model_head: Optional[Union[SetFitHead, LogisticRegression]] = None,
+        multi_target_strategy: Optional[str] = None,
+        normalize_embeddings: bool = False,
+        labels: Optional[List[str]] = None,
+        model_card_data: Optional[SetFitModelCardData] = None,
+        sentence_transformers_kwargs: Optional[Dict] = None,
+        **kwargs,
+    ) -> None:
+        super(SetFitModel, self).__init__()
+        self.model_body = model_body
+        self.model_head = model_head
+        self.multi_target_strategy = multi_target_strategy
+        self.normalize_embeddings = normalize_embeddings
+        self.labels = labels
+        self.model_card_data = model_card_data or SetFitModelCardData()
+        self.sentence_transformers_kwargs = sentence_transformers_kwargs or {}
 
-    attributes_to_save: Set[str] = field(
-        init=False, repr=False, default_factory=lambda: {"normalize_embeddings", "labels"}
-    )
-
-    def __post_init__(self):
+        self.attributes_to_save: Set[str] = {"normalize_embeddings", "labels"}
         self.model_card_data.register_model(self)
 
     @property
