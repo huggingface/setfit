9d9365282292102313d0aba81268f1c2681db39c
Tom Aarsen <Cubiegamedev@gmail.com>
2024-09-18 18:00:56 +0200

    Create custom NotebookCallback subclass for embedding_loss, etc.

--- src/setfit/trainer.py
+++ src/setfit/trainer.py
@@ -11,9 +11,9 @@ from sentence_transformers.training_args import BatchSamplers
 from sklearn.preprocessing import LabelEncoder
 from torch import nn
 from transformers.integrations import CodeCarbonCallback
-from transformers.trainer_callback import DefaultFlowCallback, IntervalStrategy, ProgressCallback, TrainerCallback
+from transformers.trainer_callback import IntervalStrategy, TrainerCallback
 from transformers.trainer_utils import HPSearchBackend, default_compute_objective, number_of_arguments, set_seed
-from transformers.utils.import_utils import is_in_notebook
+from transformers.utils.notebook import NotebookProgressCallback
 
 from setfit.model_card import ModelCardCallback
 
@@ -22,7 +22,7 @@ from .integrations import default_hp_search_backend, is_optuna_available, run_hp
 from .losses import SupConLoss
 from .sampler import ContrastiveDataset
 from .training_args import TrainingArguments
-from .utils import BestRun, default_hp_space_optuna
+from .utils import BestRun, SetFitNotebookProgressCallback, default_hp_space_optuna
 
 
 if TYPE_CHECKING:
@@ -32,15 +32,6 @@ if TYPE_CHECKING:
 
 logging.set_verbosity_info()
 logger = logging.get_logger(__name__)
-
-
-DEFAULT_CALLBACKS = [DefaultFlowCallback]
-DEFAULT_PROGRESS_CALLBACK = ProgressCallback
-
-if is_in_notebook():
-    from transformers.utils.notebook import NotebookProgressCallback
-
-    DEFAULT_PROGRESS_CALLBACK = NotebookProgressCallback
 
 
 class BCSentenceTransformersTrainer(SentenceTransformerTrainer):
@@ -63,6 +54,10 @@ class BCSentenceTransformersTrainer(SentenceTransformerTrainer):
 
             if isinstance(callback, STModelCardCallback):
                 self.remove_callback(callback)
+
+            if isinstance(callback, NotebookProgressCallback):
+                self.remove_callback(callback)
+                self.add_callback(SetFitNotebookProgressCallback())
 
         def overwritten_call_event(self, event, args, state, control, **kwargs):
             for callback in self.callbacks:
