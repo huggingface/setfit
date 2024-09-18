8ace7c648e249d0bb0c9c9358913bf719ebeec2e
Tom Aarsen <Cubiegamedev@gmail.com>
2024-09-18 13:21:10 +0200

    Run 'make style'

--- src/setfit/sampler.py
+++ src/setfit/sampler.py
@@ -93,17 +93,29 @@ class ContrastiveDataset(IterableDataset):
                 self.pos_pairs.append({"sentence_1": _text, "sentence_2": text, "label": 1.0})
             elif (_label != label) and ((self.max_pairs == -1) or (len(self.neg_pairs) < self.max_pairs)):
                 self.neg_pairs.append({"sentence_1": _text, "sentence_2": text, "label": 0.0})
-            elif self.max_pairs != -1 and len(self.pos_pairs) >= self.max_pairs and len(self.neg_pairs) >= self.max_pairs:
+            elif (
+                self.max_pairs != -1
+                and len(self.pos_pairs) >= self.max_pairs
+                and len(self.neg_pairs) >= self.max_pairs
+            ):
                 break
 
     def generate_multilabel_pairs(self) -> None:
         for (_text, _label), (text, label) in shuffle_combinations(self.sentence_labels):
-            if any(np.logical_and(_label, label)) and ((self.max_pairs == -1) or (len(self.pos_pairs) < self.max_pairs)):
+            if any(np.logical_and(_label, label)) and (
+                (self.max_pairs == -1) or (len(self.pos_pairs) < self.max_pairs)
+            ):
                 # logical_and checks if labels are both set for each class
                 self.pos_pairs.append({"sentence_1": _text, "sentence_2": text, "label": 1.0})
-            elif any(np.logical_xor(_label, label)) and ((self.max_pairs == -1) or (len(self.neg_pairs) < self.max_pairs)):
+            elif any(np.logical_xor(_label, label)) and (
+                (self.max_pairs == -1) or (len(self.neg_pairs) < self.max_pairs)
+            ):
                 self.neg_pairs.append({"sentence_1": _text, "sentence_2": text, "label": 0.0})
-            elif self.max_pairs != -1 and len(self.pos_pairs) >= self.max_pairs and len(self.neg_pairs) >= self.max_pairs:
+            elif (
+                self.max_pairs != -1
+                and len(self.pos_pairs) >= self.max_pairs
+                and len(self.neg_pairs) >= self.max_pairs
+            ):
                 break
 
     def get_positive_pairs(self) -> List[Dict[str, Union[str, float]]]:
