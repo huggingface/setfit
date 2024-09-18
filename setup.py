5a952ec146a0c6d8d40bc41c08ac552a12816bd3
Tom Aarsen <Cubiegamedev@gmail.com>
2024-09-18 09:24:11 +0200

    Require installing sentence-transformers[train]

--- setup.py
+++ setup.py
@@ -12,14 +12,13 @@ MAINTAINER_EMAIL = "lewis@huggingface.co"
 INTEGRATIONS_REQUIRE = ["optuna"]
 REQUIRED_PKGS = [
     "datasets>=2.15.0",
-    "sentence-transformers>=3",
+    "sentence-transformers[train]>=3",
     "transformers>=4.41.0",
     "evaluate>=0.3.0",
     "huggingface_hub>=0.23.0",
     "scikit-learn",
     "packaging",
 ]
-TRAIN_REQUIRE = ["accelerate>=0.20.3"]
 ABSA_REQUIRE = ["spacy<3.7.6"]
 QUALITY_REQUIRE = ["black", "flake8", "isort", "tabulate"]
 ONNX_REQUIRE = ["onnxruntime", "onnx!=1.16.2", "skl2onnx"]
@@ -30,7 +29,6 @@ CODECARBON_REQUIRE = ["codecarbon<2.6.0"]
 # 2.7.* fails with AttributeError: 'EmissionsTracker' object has no attribute '_cloud'
 # 2.6.* has an accidental print statement spamming the terminal
 EXTRAS_REQUIRE = {
-    "train": TRAIN_REQUIRE,
     "optuna": INTEGRATIONS_REQUIRE,
     "quality": QUALITY_REQUIRE,
     "tests": TESTS_REQUIRE,
