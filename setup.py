1822204983a2fff8ac6ffb9b7de46d6cb327ccb4
Tom Aarsen <Cubiegamedev@gmail.com>
2025-01-10 14:49:57 +0100

    Deprecate Python 3.8

--- setup.py
+++ setup.py
@@ -79,7 +79,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
