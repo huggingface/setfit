c56bec2dc7fe1e46f65276ba9e69287c415eae71
Tom Aarsen <Cubiegamedev@gmail.com>
2025-04-01 23:19:43 +0200

    Avoid global call without assigning

--- src/setfit/logging.py
+++ src/setfit/logging.py
@@ -327,7 +327,6 @@ tqdm = _tqdm_cls()
 
 def is_progress_bar_enabled() -> bool:
     """Return a boolean indicating whether tqdm progress bars are enabled."""
-    global _tqdm_active
     return bool(_tqdm_active)
 
 
